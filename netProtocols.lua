59021aa3ba98f6b91814b14f3bb5a6500262d0d2
lasddawsadwda <76028497+lasddawsadwda@users.noreply.github.com>
2024-11-04 11:42:59 -0600

    added first 2 networking protocols

---
+++ netProtocols.lua
@@ -0,0 +1,30 @@
+local util = require("util")
+local dev = require("dev")
+local self = {}
+
+
+--Expects an object and either "send" or "receive", returns translated data
+function self.TTO(object, direction) 
+    if direction == "send" and type(object) == "table" then
+        return util.tableToStr(object)
+    elseif direction == "receive" and type(object) == "string" then 
+        return util.splitStr(object)
+    else
+        error("incorrect object type (" .. type(object) .. ") for "..direction.." operation or invalid direction specified (TTO)", 2)
+    end
+end
+
+--Expects an object and either "send" or "receive", returns translated data
+function self.TFR(object, direction) 
+    if direction == "send" and type(object) == "table" then
+        return util.tableToStr(object)
+    elseif direction == "receive" and type(object) == "string" then 
+        return util.splitStr(object)
+    else
+        error("incorrect object type (" .. type(object) .. ") for "..direction.." operation or invalid direction specified (TFR)", 2)
+    end
+end
+
+
+return self
+
